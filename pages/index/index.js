526675ecbc86fdd7cf2287d95a7fbbfe8619a6f4
ethan <190196539@qq.com>
2017-01-12 21:57:39 +0800

    api

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,2 +1,32 @@
 //index.js
-Page({})
+
+var app = getApp()
+
+Page({
+    data:{
+        ticketList:[]
+    },
+    onLoad:function(){
+        var _this = this;
+
+        wx.showToast({
+            title: '加载中',
+            icon: 'loading',
+            duration: 10000
+        })
+
+        wx.request({
+          url: app.apiBaseUrl +  "ticket",
+          data: {},
+          method: 'GET',
+          success: function(res){
+              console.log(res.data)
+             _this.setData({ticketList:res.data})
+          },
+          complete: function() {
+            wx.hideToast()
+          }
+        })
+
+    }
+})
--- pages/order/order.js
+++ pages/order/order.js
@@ -1,17 +1,88 @@
+
+var app = getApp();
+
+function loadOrder(type,callback){
+    wx.request({
+      url: app.apiBaseUrl + "order/" +type,
+      data: {},
+      method: 'GET',
+      success: function(res){
+        callback(res.data)
+      },
+      fail: function() {
+        // fail
+      },
+      complete: function() {
+        // complete
+      }
+    })
+}
+
 Page({
     data:{
         tabSelect:{
             all:"active", pay:"", take:""
-        }
+        },
+        orderList:[]
     },
+
+    onLoad:function(){
+        var _this = this;
+        loadOrder("all",function(data){
+            _this.setData({orderList:data})
+        })
+    },
+
     tapOrderList:function(event){
-        //all pay take
+
+        //all、pay、take
         var type = event.target.dataset.type
 
         //切换选中效果
-        var data={all:"", pay:"", take:"" };
+        var data={all:"", pay:"", take:""};
         data[type] = "active";
         this.setData({tabSelect:data});
+
+        var _this = this;
+
+        loadOrder(type,function(data){
+            _this.setData({orderList:data})
+        })
+
+    },
+
+    comfirmOrder:function(event){
+
+        var orderId = event.target.dataset.orderid;
+        var _this = this;
+       
+        wx.request({
+          url: app.apiBaseUrl + 'order/comfirm/'+orderId,
+          data: {},
+          method: 'POST', 
+          success: function(res){
+            if(res.data === true){
+            
+                wx.showToast({
+                    title: '确认成功',
+                    icon: 'success',
+                    duration: 2000
+                })
+
+                //修改订单状态
+                _this.data.orderList[orderId]['status'] = '已签收'
+                _this.setData({orderList:_this.data.orderList})
+            }
+
+          },
+          fail: function() {
+            // fail
+          },
+          complete: function() {
+            // complete
+          }
+        })
+
     }
 
 })
